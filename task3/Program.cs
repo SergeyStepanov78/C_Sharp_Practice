0995464e904f67d9fda66bb1f67f92624fa067d1
Sergey <rkptz@mail.ru>
2022-10-16 15:54:28 +0300

    method Random added

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-Console.Write("Введите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int number = new Random().Next(1, 100);
+Console.WriteLine(number);
 
 if (number % 2 == 0)
 {
