efb59df18e6215931d663064b2aaadd94f031b53
Sergey <rkptz@mail.ru>
2022-10-26 22:11:53 +0300

    отменил отслеживание гитом task2,3,19,21,23, hw01

--- task1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-Console.Write("Enter three-digit number: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-
-number1 = number1 / 10 % 10;
-
-Console.WriteLine($"Вторая цифра = {number1}");
