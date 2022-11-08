b9d38c8d19b702547dc9f332f5c9472c58675119
Sergey <rkptz@mail.ru>
2022-11-08 22:23:06 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+void SumDigitsInNumber(int number)
+{
+    int lastDigit = 0;
+    int sum = 0;
+    while (Math.Abs(number) > 0)
+    {
+        lastDigit = number % 10;
+        sum = sum + lastDigit;
+        number = number / 10;
+    }
+    Console.WriteLine("sum = " + sum);
+}
+SumDigitsInNumber(452);
