b482d14faf8f41a870adbb0150d4fdca56abe944
Sergey <rkptz@mail.ru>
2022-11-08 20:12:30 +0300

    task 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+void Exponentiation(int number, int power)
+{
+    int result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result = result * number;
+    }
+    Console.WriteLine(result);
+}
+Exponentiation(3, 5);
