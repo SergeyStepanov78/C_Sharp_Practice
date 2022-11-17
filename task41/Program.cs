5518389546b882d9206f13263027e49585b894e6
Sergey <rkptz@mail.ru>
2022-11-17 18:49:40 +0300

    task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -4,3 +4,34 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 4
 */
+
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] array = GetArrayFromString(elements);
+
+Console.WriteLine($"получившийся массив = [{String.Join(",", array)}]");
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+void CountNumOverZero(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"количество чисел в массиве больше 0 = {count}");
+}
+CountNumOverZero(array);
